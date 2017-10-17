e862864b9276b5c258c9109213399fcd4d42d874
Juan Cespedes <juan.cespedes@imdea.org>
2017-10-17 13:50:15 +0200

    added simple service

--- app/src/main/java/org/cespedes/android/carlog/MainActivity.java
+++ app/src/main/java/org/cespedes/android/carlog/MainActivity.java
@@ -2,11 +2,22 @@ package org.cespedes.android.carlog;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
+    }
+
 }
